82a3d13c7fcec975ba11f5279f2fb0c3167d4b41
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-09 17:49:23 +0200

    bcrpt hash password on user create

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,31 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const router = express.Router()
 const db = require(process.env.MOCKS ? '../db/db-mocks.js' : '../db/db-sql.js')
 
+const prepareUser = user => ({
+  id: user.id,
+  createdAt: user.createdAt,
+  username: user.username,
+  isAdmin: Boolean(user.isAdmin)
+})
+const prepareUsers = users => users.map(prepareUser)
+
 router.get('/users', (req, res, next) => {
   db.getUsers()
+    .then(prepareUsers)
     .then(users => res.json(users))
     .catch(next)
 })
 
-router.post('/users', (req, res, next) => {
-  const user = req.body
+router.post('/users', async (req, res, next) => {
+  const saltRounds = 10
+  const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+
+  const user = {
+    ...req.body,
+    password: hashedPassword
+  }
 
   db.newUser(user)
     .then(() => res.json('ok'))
