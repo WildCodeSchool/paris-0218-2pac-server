8698a9a31335dda80eb8ed03d63bbd48f13719ef
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-10 18:17:15 +0200

    authRequired error message updated

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,13 +1,19 @@
 const authRequired = (req, res, next) => {
-  if (!req.user) { return res.status(401).json('Authentication required') }
+  if (!req.user) {
+    return res.status(401).json({ error: 'Authentication required' })
+  }
 
   next()
 }
 
 authRequired.asAdmin = (req, res, next) => {
-  if (!req.user) { return res.status(401).json('Authentication required') }
+  if (!req.user) {
+    return res.status(401).json({ error: 'Authentication required' })
+  }
 
-  if (!req.user.isAdmin) { return res.status(401).json('Admin authentication required') }
+  if (!req.user.isAdmin) {
+    return res.status(401).json({ error: 'Admin authentication required' })
+  }
 
   next()
 }
