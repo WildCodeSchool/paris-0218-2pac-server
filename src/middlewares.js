87764b0ac4009854911f5371751c9317c04e0d64
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-10 10:09:35 +0200

    authRequired.asAdmin middleware added

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,6 +4,14 @@ const authRequired = (req, res, next) => {
   next()
 }
 
+authRequired.asAdmin = (req, res, next) => {
+  if (!req.user) { return res.status(401).end() }
+
+  if (!req.user.isAdmin) { return res.status(401).json('Admin role required') }
+
+  next()
+}
+
 module.exports = {
   authRequired,
 }
