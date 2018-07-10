32a2bc48e2e95911fcd6619bbdf7881208ec6508
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-10 10:19:52 +0200

    auth middlewares error messages update

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,13 +1,13 @@
 const authRequired = (req, res, next) => {
-  if (!req.user) { return res.status(401).end() }
+  if (!req.user) { return res.status(401).json('Authentication required') }
 
   next()
 }
 
 authRequired.asAdmin = (req, res, next) => {
-  if (!req.user) { return res.status(401).end() }
+  if (!req.user) { return res.status(401).json('Authentication required') }
 
-  if (!req.user.isAdmin) { return res.status(401).json('Admin role required') }
+  if (!req.user.isAdmin) { return res.status(401).json('Admin authentication required') }
 
   next()
 }
