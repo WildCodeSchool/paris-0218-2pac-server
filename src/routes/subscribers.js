2437dbd4f085a9387211f8d6639dc0a860c9a81d
Helene Blanchard <erba0614@gmail.com>
2018-07-10 17:20:15 +0200

    ajout de la gestion des Infos réutilisables dans le composant Subscriber

--- src/routes/subscribers.js
+++ src/routes/subscribers.js
@@ -2,14 +2,24 @@ const express = require('express')
 const router = express.Router()
 const db = require(process.env.MOCKS ? '../db/db-mocks.js' : '../db/db-sql.js')
 
-// récupération des données utilisateur
+
+const prepareSubscriber = subscriber => ({
+  ...subscriber,
+  reuseableInfo: Boolean(subscriber.reuseableInfo)
+})
+const prepareSubscribers = subscribers => subscribers.map(prepareSubscriber)
+
+
+// récupération des données abonnés
 router.get('/subscribers', (req, res, next) => {
   db.getSubscribers()
+    .then(prepareSubscribers)
     .then(subscribers => res.json(subscribers))
     .catch(next)
 })
 
-// envoi des données utilisateur
+
+// envoi des données abonnés
 router.post('/subscribers', (req, res, next) => {
   const subscriber = req.body
   console.log(subscriber)
