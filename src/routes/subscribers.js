c580c888a67e0afc480688d8a2a281c748c196a2
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-11 21:46:43 +0200

    subscribers mocks added + prepare

--- src/routes/subscribers.js
+++ src/routes/subscribers.js
@@ -3,14 +3,19 @@ const router = express.Router()
 const { authRequired } = require('../middlewares.js')
 const db = require(process.env.MOCKS ? '../db/db-mocks.js' : '../db/db-sql.js')
 
-// récupération des données utilisateur
+const prepareSubscriber = subscriber => ({
+  ...subscriber,
+  reuseableInfo: Boolean(subscriber.reuseableInfo)
+})
+const prepareSubscribers = subscribers => subscribers.map(prepareSubscriber)
+
 router.get('/subscribers', authRequired.asAdmin, (req, res, next) => {
   db.getSubscribers()
+    .then(prepareSubscribers)
     .then(subscribers => res.json(subscribers))
     .catch(next)
 })
 
-// envoi des données utilisateur
 router.post('/subscribers', authRequired.asAdmin, (req, res, next) => {
   const subscriber = req.body
   console.log(subscriber)
