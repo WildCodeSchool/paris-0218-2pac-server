03c65bbef5d53ec144d5515817a72559e020a61d
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-24 11:30:05 +0200

    init.sql - clean

--- src/db/init.sql
+++ src/db/init.sql
@@ -2,16 +2,6 @@ DROP DATABASE IF EXISTS pac;
 CREATE DATABASE pac CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE pac;
 
--- TODOS:
--- - tags table
--- - pinnedArticles table
-
--- CREATE TABLE tags
--- (
--- id INT PRIMARY KEY AUTO_INCREMENT,
--- name VARCHAR(50)
--- )
-
 CREATE TABLE articles_categories
 (
 categoryId INT PRIMARY KEY AUTO_INCREMENT,
@@ -41,7 +31,6 @@ FOREIGN KEY (categoryId)
   REFERENCES articles_categories(categoryId)
   ON DELETE CASCADE
 );
--- TODO: FOREIGN keys (O:M resources; O:M tags)
 
 CREATE TABLE documents_types
 (
@@ -68,7 +57,6 @@ FOREIGN KEY (typeId)
   REFERENCES documents_types(typeId)
   ON DELETE CASCADE
 );
--- TODO: FOREIGN keys (O:M tags)
 
 CREATE TABLE users
 (
@@ -78,11 +66,6 @@ isAdmin BOOLEAN DEFAULT 0,
 username VARCHAR(30) NOT NULL UNIQUE,
 password VARCHAR(100) NOT NULL
 );
-
--- ADMIN ACCOUNT
-INSERT INTO users (username, password, isAdmin) VALUES ("admin", "$2b$10$Iae4rGF5t4Fm.RPVDDabSuZNzWg97q5sqXEl/aoP4JiVpIufwBhVu", 1);
--- MEMBER ACCOUNT
-INSERT INTO users (username, password, isAdmin) VALUES ("member", "$2b$10$OHIeQ/eY1UvjzRDVDtofmOBl5l9Sq6ix8hW2mnOgl1vcMCnCt9.gq", 0);
 
 CREATE TABLE subscribers
 (
